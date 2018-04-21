a7fb0e4872699b14b842c8d507b7dc11eedd279b
acoustic_jeet <js.jitendra11@gmail.com>
2018-04-21 22:55:57 +0530

    Add files via upload

---
+++ colorGame.js
@@ -0,0 +1,44 @@
+var colors = [
+"rgb(255, 0, 0)",
+"rgb(255, 255, 0)",
+"rgb(0, 255, 0)",
+"rgb(0, 255, 255)",
+"rgb(0, 0, 255)",
+"rgb(255, 0, 255)"
+]
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+
+colorDisplay.textContent = pickedColor;
+
+for(var i = 0; i < squares.length; i++){
+	//add initial colors to squares
+	squares[i].style.backgroundColor = colors[i];
+
+	//add click listeners to squares.
+	squares[i].addEventListener("click", function(){
+		// alert("clicked a square");
+		//grab color of clicked square
+		var clickedColor = this.style.backgroundColor;
+		//Compare color to pickedColor
+		if(clickedColor === pickedColor){
+			messageDisplay.textContent = "Correct!";
+			changeColors(clickedColor);
+		}else{
+			alert("Wrong!!!");
+			this.style.backgroundColor = "#232323"
+			messageDisplay.textContent = "Try Again";
+		}
+	});
+}
+
+function changeColors(color){
+	//loo through all squres
+	for(var i = 0; i < squares.length; i++){
+		squares[i].style.backgroundColor = color;
+	}
+	//change each color to match given color
+}
